c1147ec099988cec4907a9c0cab7ddb457ff14ac
Isur <bednarczykartur96@gmail.com>
2019-06-10 22:24:13 +0200

    componentDidMount init react ga

--- src/client/Pages/App.tsx
+++ src/client/Pages/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Header } from "semantic-ui-react";
+import ReactGA from "react-ga";
+import config from "../../config";
 import TestComponent from "../Components/TestComponent";
 import "./App.scss";
 
@@ -8,6 +10,11 @@ interface IState {}
 interface IProps {}
 
 export default class App extends React.Component<IProps, IState> {
+  componentDidMount() {
+    ReactGA.initialize(config.googleTrackerID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   handleFunc = (): void => {
     alert("Hi!");
   }
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import ReactGA from "react-ga";
-import config from "../config";
 import App from "./Pages/App";
-
-ReactGA.initialize(config.googleTrackerID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("react-app"));
