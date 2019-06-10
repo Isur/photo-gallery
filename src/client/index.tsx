bd05850d8710ed652ff98208a7a4cdb25c41a39e
Isur <bednarczykartur96@gmail.com>
2019-06-10 22:01:40 +0200

    Google Analytics

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+import config from "../config";
 import App from "./Pages/App";
+
+ReactGA.initialize(config.googleTrackerID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("react-app"));
